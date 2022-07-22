644f1f24cc7d989332c35a030c3b615896566bf8
Ian Mota <ianmotabr@gmail.com>
2022-07-21 21:51:44 -0300

    api-getWorkspace-finished-00

--- core/teamProductive.py
+++ core/teamProductive.py
@@ -1,5 +1,17 @@
 import pandas
 
 class ProdutividadeEquipe():
-    def __init__(self) -> None:
-        pass
+    def __init__(self,data_base:dict) -> None:
+        self.database = data_base["tasks"]
+        
+    def getTask(self):
+        return(self.database["name"])
+    
+    def getCreatedDate(self):
+        return(self.database["date_created"])
+    
+    def getUpdatedDate(self):
+        return(self.database["date_updated"])
+    
+    def getDataClosed(self):
+        return(self.database["date_closed"])
