80f968bd048a7d124f314d56c2027b2840d52523
Ian Mota <ianmotabr@gmail.com>
2022-07-21 21:59:28 -0300

    api-getSpace-finished-00

--- service/getSpaces.py
+++ service/getSpaces.py
@@ -2,21 +2,33 @@ import requests
 
 class Spaces():
     def __init__(self,auth:dict,space_name:str,workspace_id:int) -> None:
+        """Coleta os dados de todos os espaços
+        Args:
+            auth:{Authorization:token}
+            space_name: template
+            workspace_id: 0912
+        """
         self.auth = auth
-        self.spaceName = space_name.upper()
+        self.spaceName = space_name.upper().replace(" ","")
         self.workspaceID = workspace_id
+        self.endpoint = f"https://api.clickup.com/api/v2/team/{self.workspaceID}/space?archived=false"
         
-    def getSpaces(self):
-        endpoint = f"https://api.clickup.com/api/v2/team/{self.workspaceID}/space?archived=false"
-        spaces = requests.get(endpoint,headers=self.auth)
+    def getSpaces(self)->dict:
+        """retorna todos os espaços do usuário
+        """
+        spaces = requests.get(self.endpoint,headers=self.auth)
         spaces_json = spaces.json()
         return(spaces_json["spaces"])
 
-    def getSpaceID(self):
+    def getSpaceID(self)->int:
+        """retorna o id do espaço procurado
+        """
         space = self.getSpaces()
-        
+
         for i in range(len(space)):
-            if(space[i]["name"] == self.spaceName):
+            space_generate = space[i]["name"]
+            space_name = space_generate.upper().replace(" ","")
+            if( space_name == self.spaceName):
                 spaceID = space[i]["id"]
                 break
             else:
@@ -24,7 +36,9 @@ class Spaces():
                 
         return(spaceID)
     
-    def getSpacesStatus(self):
+    def getSpacesStatus(self)->int:
+        """retorna o status da solicitação a api
+        """
         endpoint = f"https://api.clickup.com/api/v2/team/{self.workspaceID}/space?archived=false"
         spaces = requests.get(endpoint,headers=self.auth)
         return(spaces.status_code)
